91e1e3e3934dd6df41328a87cf07a8b8d7cac1a1
Vahn Gomes <me@vahngomes.dev>
2024-04-07 20:06:03 -0400

    Basic web

---
+++ web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Discuit Search</h1>
+  </header>
+  <main>
+    <input
+      hx-get="/search"
+      hx-target="#results"
+      hx-include="this"
+      hx-trigger="keyup changed"
+      name="q"
+      type="text"
+      placeholder="Search..."
+    />
+    <ul id="results"></ul>
+  </main>
+</template>
---
+++ web/src/components/CommunityComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Community {
+  id: number
+  name: string
+  about: string | null
+}
+const community: Community = defineProps<{ community: Community }>().community
+</script>
+
+<template>
+  <a :href="`https://discuit.net/${community.name}`">
+    <li>
+      <h3>{{ community.name }}</h3>
+      <p :inner-h-t-m-l="community.about"></p>
+    </li>
+  </a>
+</template>
+
+<style scoped>
+li {
+  list-style-type: none;
+  border: 0.125rem solid canvastext;
+  margin: 0.5rem 0;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: all 0.25s;
+
+  &:hover {
+    background-color: #444;
+  }
+}
+
+p {
+  word-break: break-word;
+}
+
+a {
+  text-decoration: none;
+  color: canvastext;
+  height: 100%;
+}
+</style>
